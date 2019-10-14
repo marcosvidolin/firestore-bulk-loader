21c86ae47957afd6b00ad73d56b62725c90699dc
Marcos Vidolin <marcosvidolin@gmail.com>
2019-10-14 01:42:33 -0300

    Basic code

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World!!');
+const admin = require('firebase-admin');
+const serviceAccount = require("./private/credentials/service-account.json");
+const data = require("./private/data/data.json");
+const collectionKey = "my-collection"; //name of the collection
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+    // , databaseURL: "https://your-database.firebaseio.com"
+});
+
+const firestore = admin.firestore();
+const settings = {
+    timestampsInSnapshots: true
+};
+
+firestore.settings(settings);
+
+if (data && (typeof data === "object")) {
+    Object.keys(data).forEach(docKey => {
+        firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+            console.log("Document " + docKey + " successfully written!");
+        }).catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    });
+}

65899112f0e0dc21dea108b5071d25aa0fbc7bbb
Marcos Vidolin <marcosvidolin@gmail.com>
2019-10-17 18:57:20 -0300

    Basic functions

--- index.js
+++ index.js
@@ -1,26 +1,106 @@
+/*!
+ * firestore-bulk-loader
+ * Copyright(c) 2019 Marcos A. Vidolin de Lima
+ * MIT Licensed
+ */
+"use strict";
+
 const admin = require('firebase-admin');
-const serviceAccount = require("./private/credentials/service-account.json");
-const data = require("./private/data/data.json");
-const collectionKey = "my-collection"; //name of the collection
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-    // , databaseURL: "https://your-database.firebaseio.com"
-});
+let firestore = undefined;
 
-const firestore = admin.firestore();
-const settings = {
-    timestampsInSnapshots: true
+/**
+ * Configures the Firestore object.
+ * 
+ * @param {string} serviceAccount
+ * @returns {Object} firestore
+ */
+const configureFirestore = (serviceAccount) => {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    firestore = admin.firestore();
+    firestore.settings({
+        timestampsInSnapshots: true
+    });
+
+    return firestore;
 };
 
-firestore.settings(settings);
+/**
+ * Converts the given data into valid documents to be imported.
+ * 
+ * @param {*} data 
+ * @returns documents
+ */
+const checkData = (data) => {
+    if (!data) {
+        throw new Error("No data to load.");
+    }
+    if (!Array.isArray(data)) {
+        throw new Error("Invalid data. You must inform an array.");
+    }
+};
 
-if (data && (typeof data === "object")) {
+/**
+ * Loads the data with a specific ID.
+ * 
+ * @param {*} data 
+ * @param {*} collectionKey 
+ * @param {*} documentKeyProperty 
+ */
+const loadDataWithId = (data, collectionKey, documentKeyProperty) => {
     Object.keys(data).forEach(docKey => {
-        firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
-            console.log("Document " + docKey + " successfully written!");
-        }).catch((error) => {
-            console.error("Error writing document: ", error);
+        let document = data[docKey];
+        let id = document[documentKeyProperty];
+        firestore.collection(collectionKey).doc(id).set(document).then((ref) => {
+            console.log('Added document with ID: ', id);
         });
     });
-}
+};
+
+/**
+ * Loads the data without ID. Google Firestore will auto generate the ID.
+ * 
+ * @param {*} documents 
+ * @param {*} collectionKey 
+ */
+const loadDataWithoutId = (documents, collectionKey) => {
+    Object.keys(documents).forEach(doc => {
+        firestore.collection(collectionKey).add(documents[doc]).then(ref => {
+            console.log('Added document with ID: ', ref.id);
+        });
+    });
+};
+
+/**
+ * A simple Firestore loader.
+ * 
+ * @param {*} data 
+ * @param {*} collectionKey 
+ * @param {*} serviceAccount 
+ * @param {*} options 
+ */
+const loadData = function (data, collectionKey, serviceAccount, options) {
+
+    checkData(data);
+
+    let opts = options || {};
+    const docKeyProperty = opts.documentKeyProperty;
+
+    configureFirestore(serviceAccount);
+
+    if (docKeyProperty) {
+        loadDataWithId(data, collectionKey, docKeyProperty);
+    } else {
+        loadDataWithoutId(data, collectionKey);
+    }
+};
+
+/**
+ * Exports.
+ */
+module.exports = {
+    load: loadData
+};
